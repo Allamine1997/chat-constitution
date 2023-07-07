1fd4fddea8e62f2a3dfbb26c950b07ee65c0bb29
Toadoum <toadoum.sari@aims-cameroon.org>
2023-07-08 01:14:01 +0200

    add files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chatbot
+
+Chatbot Chadian Constitution of 2018 using `LangChain`, `Pinecone`, `ChatGPT`, and `Streamlit`.
+
+NB: My open ai API Key is no longer working because my free plan have expire. Someone with a valid key can try this and it will work.
---
+++ app.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import openai
+from pinecone import PineconeProtocolError
+from streamlit_chat import message
+from utils import get_conversation_string, find_match, query_refiner
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
+
+
+st.title("Constitution du Tchad de 2018")
+
+
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
+
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+if 'buffer_memory' not in st.session_state:
+    st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+
+
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully 
+as possible using the provided context, and if the answer is not contained within the text below, say 'I don't know'""")
+
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), 
+                                                    human_msg_template])
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, 
+                                 llm=llm, verbose=True)
+
+
+
+textcontainer = st.container()
+response_container = st.container()
+
+try:
+    with textcontainer:
+        query = st.text_input("Query: ", key="input")
+        if query:
+            with st.spinner("typing..."):
+                conversation_string = get_conversation_string()
+                # st.code(conversation_string)
+
+                refined_query = query_refiner(conversation_log=conversation_string,query=query)
+                st.subheader('Refined Query:')
+                st.write(refined_query)
+
+                context = find_match(refined_query)
+                # st.code(context)
+
+                response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+
+            st.session_state.requests.append(query)
+            st.session_state.responses.append(response)
+
+
+    with response_container:
+        if st.session_state['responses']:
+            for i in range(len(st.session_state['responses'])):
+                message(st.session_state['responses'][i],key=str(i))
+                if i < len(st.session_state['requests']):
+                    message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+
+except openai.error.AuthenticationError:
+    errormessage = "Please provide a valid OpenAI API Key."
+    st.header(errormessage)
+
+except PineconeProtocolError:
+    errormessage = "Please check the Pinecone API Key and/or Pinecone Index."
+    st.header(errormessage)
+
+except Exception as e:
+    st.header (e.__class__.__name__)
+
